af7a4b6f05a64a0768a867bbb2f1cd0e640439b1
Abu Osama <s.islam20000@gmail.com>
2024-11-12 13:19:03 +0530

    todo initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import React from "react";
-import Todos from "../components/Todos";
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-4xl text-center text-black bg-gradient-to-r from-purple-500 to-blue-500 shadow-lg p-6">
-        Welcome to todo App
-      </h2>
-      <Todos />;
-    </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
   );
 }
